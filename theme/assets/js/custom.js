336984cc09d75c3268927f6262f71dbe17b6fb6e
aveekdeveloper <aveekdeveloper@gmail.com>
2014-01-11 15:51:27 +0530

    fixed scroll problem

--- theme/assets/js/custom.js
+++ theme/assets/js/custom.js
@@ -23,3 +23,12 @@ map.on('locationfound', onLocationFound);
 map.on('locationerror', onLocationError);
 
 map.locate({setView: true, maxZoom: 16});
+
+/*
+$( '#review' ).bind( 'mousewheel DOMMouseScroll', function ( e ) {
+    var e0 = e.originalEvent,
+        delta = e0.wheelDelta || -e0.detail;
+    
+    this.scrollTop += ( delta < 0 ? 1 : -1 ) * 30;
+    e.preventDefault();
+});*/
